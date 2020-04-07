f02c152c11823d2615eef7841fc1d844851948b2
mkielar <mkielarv2@gmail.com>
2020-04-07 19:05:49 +0200

    added image loading

--- app/src/main/java/com/mkielar/sklepallegro/DetailsFragment.kt
+++ app/src/main/java/com/mkielar/sklepallegro/DetailsFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_details.*
 
 /**
@@ -26,7 +27,11 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
             arguments?.also {
                 val offerDTO = DetailsFragmentArgs.fromBundle(it).offer
+                (activity as MainActivity).supportActionBar?.title = offerDTO.name
                 webView.loadData(offerDTO.description, "text/html", "UTF-8")
+                Glide.with(this)
+                    .load(offerDTO.thumbnailUrl)
+                    .into(thumbnail)
             }
 
     }
--- app/src/main/java/com/mkielar/sklepallegro/OfferViewHolder.kt
+++ app/src/main/java/com/mkielar/sklepallegro/OfferViewHolder.kt
@@ -1,15 +1,22 @@
 package com.mkielar.sklepallegro
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mkielar.sklepallegro.dto.OfferDTO
 
 class OfferViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(offerDTO: OfferDTO) {
-        view.findViewById<TextView>(R.id.mainText).text = offerDTO.name
+        view.findViewById<TextView>(R.id.name).text = offerDTO.name
+        view.findViewById<TextView>(R.id.price).text = "${offerDTO.price.amount} ${offerDTO.price.currency}"
+        val thumbnailView = view.findViewById<ImageView>(R.id.thumbnail)
+        Glide.with(view.context)
+            .load(offerDTO.thumbnailUrl)
+            .into(thumbnailView)
         view.findViewById<ConstraintLayout>(R.id.container).setOnClickListener {
             it.findNavController().navigate(
                 MainFragmentDirections.actionMainFragmentToDetailsFragment(offerDTO)
